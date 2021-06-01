da2db9cbdf7cb10029bb4f35992cc0a0ebcd0443
poonnatuch B <poonnatuch@siam-u.ac.th>
2021-06-01 13:21:17 +0700

    Initial commit

---
+++ src/main/java/model/Model.java
@@ -0,0 +1,32 @@
+package model;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import model.Model;
+import java.util.List;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * UserModel
+ * 
+ * Provide all Model's sub classes a connection with DB.
+ */
+public abstract class Model {
+
+    public final String spreadsheetId = "1DsQ-bpTeJu_PMT9nR5Eo23uN1vEx2OFuVUylkY7XBLs";
+
+    private DB db = new DB();
+    public Sheets connection;
+
+    public Model() {
+        try {
+            this.connection = db.getInstance();
+        } catch(Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+        
+    }
+}
